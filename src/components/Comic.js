4070c991e67abba889ca377b8d5f29c2da4fbefd
Cathy T Bradley <cathytbradley116@gmail.com>
2021-12-16 17:29:59 -0800

    add card styling to comic page

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -1,4 +1,5 @@
 import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+import { Grid, Card, CardHeader, CardActionArea, CardContent } from '@material-ui/core';
 
 const Comic = () => {
 
@@ -6,9 +7,13 @@ const Comic = () => {
 
   const { status, data } = useFirestoreCollectionData(comicRef);
 
+  const style = {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
   
   if (status === 'loading') {
-    console.log(status)
     return (
       <div>
         loading...
@@ -17,19 +22,28 @@ const Comic = () => {
   } else {
     return ( 
     <div>
-      <h1>Comics Page</h1>
-      {data.map((comic) => (
-        <div>
-          <p>{comic.title}</p>
-          <img src={comic.titleUrl}/>
-          <p>{comic.description}</p>
-        </div>
-      ))}
-    </div> );
+      <h1>All the Comics Stuff</h1>
+      <Grid container spacing={3}>
+        {data.map((comic) => (
+          <Grid item xs={4}>
+            <CardActionArea>
+              <Card>
+                <CardHeader title={comic.title}/>
+                <div>
+                  <img src={comic.titleUrl} style={style}/>
+                </div>
+                <CardContent>
+                  {comic.description}
+                </CardContent>
+              </Card>
+            </CardActionArea>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+    );
   }
 
 }
  
 export default Comic;
-
-// display an image of the comic with a name below
