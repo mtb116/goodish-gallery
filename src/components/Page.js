6e9da6ef0cf50906415e8732873e5399e0a90705
Cathy T Bradley <cathytbradley116@gmail.com>
2022-01-07 16:23:53 -0800

    add forward/back buttons on comic page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams, useRouteMatch } from "react-router-dom"
 import { makeStyles } from '@material-ui/core';
 
 const Page = (props) => {
@@ -7,26 +7,38 @@ const Page = (props) => {
     pages
   } = props
 
-  let { pageNum } = useParams();
+  let { pageNum, id } = useParams();
+  let { path, url } = useRouteMatch();
+  console.log('path ' + path)
+  console.log('url ' + url)
+  console.log(id)
 
   const pageArr = pages.filter(page => page.page == pageNum);
   const page = pageArr[0]
   console.log(page)
 
   const makeCenter = makeStyles({
-    img: {
+    content: {
       display: 'block',
       marginLeft: 'auto',
       marginRight: 'auto',
       width: '50%'
+    },
+    text: {
+      textAlign: 'center'
     }
   });
 
   const center = makeCenter();
 
   return ( 
-    <div>
-      <img src={page.pageUrl} alt='' className={center.img}/>
+    <div className={center.content}>
+      <div>
+        <Link to={`/${id}/${parseInt(pageNum) + 1}`}>next page</Link>
+        <p className={center.text}>{page.page}</p>
+        <Link to={`/${id}/${parseInt(pageNum) - 1}`}>previous page</Link>
+      </div>
+      <img src={page.pageUrl} alt=''/>
     </div>
 
   );
